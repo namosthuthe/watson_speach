b7763a91c7d260ac197b54d69daabfc0fce9a42b
diabolicfreak <diabolicfreak@gmail.com>
2018-03-27 17:06:09 +0530

    env file fix

--- views/watsonbody.jsx
+++ views/watsonbody.jsx
@@ -1,4 +1,3 @@
-/* eslint no-param-reassign: 0 */
 import React from 'react';
 import Dropzone from 'react-dropzone';
 import recognizeMicrophone from 'watson-speech/speech-to-text/recognize-microphone';
@@ -32,10 +31,6 @@ export default React.createClass({
     this.setState({ rawMessages: [], formattedMessages: [], error: null });
   },
 
-  /**
-     * The behavior of several of the views depends on the settings when the
-     * transcription was started. So, this stores those values in a settingsAtStreamStart object.
-     */
   captureSettings() {
     this.setState({
       settingsAtStreamStart: {
@@ -53,14 +48,12 @@ export default React.createClass({
 
   getRecognizeOptions(extra) {
     return Object.assign({
-      // formats phone numbers, currency, etc. (server-side)
       token: this.state.token,
       smart_formatting: true,
       format: true, // adds capitals, periods, and a few other things (client-side)
       model: this.state.model,
       objectMode: true,
       interim_results: true,
-      // note: in normal usage, you'd probably set this a bit higher
       word_alternatives_threshold: 0.01,
       timestamps: true, // set timestamps for each word - automatically turned on by speaker_labels
     }, extra);
@@ -94,21 +87,15 @@ export default React.createClass({
     this.setState({ error: 'Sorry, that file does not appear to be compatible.' });
   },
 
-  /**
-   * @param {File|Blob|String} file - url to an audio file or a File
-   * instance fro user-provided files.
-   */
   playFile(file) {
     this.handleStream(recognizeFile(this.getRecognizeOptions({
       file,
       play: true, // play the audio out loud
-      // use a helper stream to slow down the transcript output to match the audio speed
       realtime: true,
     })));
   },
 
   handleStream(stream) {
-    // cleanup old stream if appropriate
     if (this.stream) {
       this.stream.stop();
       this.stream.removeAllListeners();
@@ -117,11 +104,7 @@ export default React.createClass({
     this.stream = stream;
     this.captureSettings();
 
-    // grab the formatted messages and also handle errors and such
     stream.on('data', this.handleFormattedMessage).on('end', this.handleTranscriptEnd).on('error', this.handleError);
-
-    // when errors occur, the end event may not propagate through the helper streams.
-    // However, the recognizeStream should always fire a end and close events
     stream.recognizeStream.on('end', () => {
       if (this.state.error) {
         this.handleTranscriptEnd();
@@ -136,18 +119,11 @@ export default React.createClass({
   },
 
   handleTranscriptEnd() {
-    // note: this function will be called twice on a clean end,
-    // but may only be called once in the event of an error
     this.setState({ audioSource: null });
   },
 
   componentDidMount() {
     this.fetchToken();
-    // tokens expire after 60 minutes, so automatcally fetch a new one ever 50 minutes
-    // Not sure if this will work properly if a computer goes to sleep for > 50 minutes
-    // and then wakes back up
-    // react automatically binds the call to this
-    // eslint-disable-next-line
     this.setState({ tokenInterval: setInterval(this.fetchToken, 50 * 60 * 1000) });
   },
 
@@ -185,10 +161,6 @@ export default React.createClass({
 
   getCurrentInterimResult() {
     const r = this.state.formattedMessages[this.state.formattedMessages.length - 1];
-
-    // When resultsBySpeaker is enabled, each msg.results array may contain multiple results.
-    // However, all results in a given message will be either final or interim, so just checking
-    // the first one still works here.
     if (!r || !r.results || !r.results.length || r.results[0].final) {
       return null;
     }
@@ -220,9 +192,6 @@ export default React.createClass({
   },
 
   render() {
-    // const btnStyle = {
-    //   padding: 10px
-    // }
 
     const err = this.state.error
       ? (
